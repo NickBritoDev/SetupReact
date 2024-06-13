1b4a54cf8feb0bf7a5935441779eec73b937ff4f
NickBritoDev <nicolasbcruz@gmail.com>
2024-06-13 11:47:20 -0300

    feat(husky): teste de padronização de codigo com prettier, husky, commit lint, commitzen, etc

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,30 +1,32 @@
-import { Navigate, useRoutes } from "react-router-dom";
-import LayoutAdmin from "./layout/admin";
-import LayoutPublic from "./layout/public";
-import Home from "./pages/admin/home";
-import NaoAutorizado401 from "./pages/public/nao-autorizado";
-import NaoLocalizado404 from "./pages/public/nao-localizado";
+import { Navigate, useRoutes } from 'react-router-dom';
+import LayoutAdmin from './layout/admin';
+import LayoutPublic from './layout/public';
+import Home from './pages/admin/home';
+import NaoAutorizado401 from './pages/public/nao-autorizado';
+import NaoLocalizado404 from './pages/public/nao-localizado'
 
 export default function Routes() {
   const auth = false;
   const routing = useRoutes([
     {
-      path: "/admin",
+      path: '/admin',
       element: <LayoutAdmin />,
       children: [
         {
-          path: "home",
+          path: 'home',
           element: auth ? <Home /> : <NaoAutorizado401 />,
         },
       ],
     },
     {
-      path: "/public",
+      path: '/public',
       element: <LayoutPublic />,
-      children: [{ path: "nao-localizado", element: <NaoLocalizado404 /> }],
+      children: [
+        { path: 'nao-localizado', element: <NaoLocalizado404 /> },
+      ],
     },
-    { path: "*", element: <Navigate to="/public/nao-localizado" replace /> },
-    { path: "/", element: <Navigate to="/admin/home" replace /> },
+    { path: '*', element: <Navigate to='/public/nao-localizado' replace /> },
+    { path: '/', element: <Navigate to='/admin/home' replace /> },
   ]);
 
   return routing;
