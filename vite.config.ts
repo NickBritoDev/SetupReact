3675b329baadd0e9fed0b16514edf02f07326a2e
Otavio Tampellini <tampelliniotavio@gmail.com>
2024-07-05 17:33:09 -0300

    build: adicionando import com @

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+
+const resolve = (name: string) => path.resolve(__dirname, `./src/${name}/`);
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@api": resolve("api"),
+      "@components": resolve("components"),
+      "@helpers": resolve("helpers"),
+      "@layout": resolve("layout"),
+      "@pages": resolve("pages"),
+      "@themes": resolve("themes"),
+      "@context": resolve("context"),
+      "@utils": resolve("utils"),
+      "@modules": resolve("modules"),
+      "@hooks": resolve("hooks"),
+    },
+  },
   plugins: [react()],
 });
