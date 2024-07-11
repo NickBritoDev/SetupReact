74351b53c2f53ef26f496e0d0b73fbddb0f46da5
Nicolas Brito da Cruz <nicolas.cruz@GMVB.LOCAL>
2024-07-11 09:59:58 -0300

    feat(gerenciador): link de download do gerenciador de instancias
    executavel local para maior desempenho e economia
    
    BREAKING CHANGE: melohorar a execução das leituras de qr code das instancias

--- src/modules/admin/crm-leads-bot/components/whatsapp/dialogWhatsapp.tsx
+++ src/modules/admin/crm-leads-bot/components/whatsapp/dialogWhatsapp.tsx
@@ -76,7 +76,6 @@ export default function DialogWhatsappComponent({
           }}
           ref={btnRef}
         >
-          <Text>WhatsApp</Text>
           <SiWhatsapp size={22} />
         </Button>
       </Tooltip>
