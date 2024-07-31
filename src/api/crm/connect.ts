ed1d920e6377b219f306a0ca4480d3cc20cb2c30
Nicolas Brito da Cruz <nicolas.cruz@GMVB.LOCAL>
2024-07-31 17:44:15 -0300

    fix(connect crm): estava em localhost

--- src/api/crm/connect.ts
+++ src/api/crm/connect.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const connectCrm = axios.create({
-  // baseURL: "https://apicrm.portalmaisvalor.com",
-  baseURL: "http://localhost:5059/",
+  baseURL: "https://apicrm.portalmaisvalor.com",
+  // baseURL: "http://localhost:5059/",
 });
 
 export const testInstance = axios.create({
