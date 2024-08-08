e52158cba58489a5f39a5c0dd49370c891aad5aa
Nicolas Brito da Cruz <nicolas.cruz@GMVB.LOCAL>
2024-08-08 10:38:58 -0300

    feat(login): remoção de login

--- src/routes.tsx
+++ src/routes.tsx
@@ -20,7 +20,7 @@ import { useToast } from "@chakra-ui/react";
 import RelatoriosFinalizadosCrm from "@modules/admin/relatorios/leads-finalizados";
 import RelatoriosPorUsuarioCrm from "@modules/admin/relatorios/leads-por-usuario";
 import RelatoriosRecebidosCrm from "@modules/admin/relatorios/leads-recebidos";
-import Login from "@pages/public/login";
+// import Login from "@pages/public/login";
 
 const Routes: React.FC = () => {
   const location = useLocation();
@@ -180,7 +180,7 @@ const Routes: React.FC = () => {
       path: "/public",
       element: <LayoutPublic />,
       children: [
-        { path: "login", element: <Login /> },
+        // { path: "login", element: <Login /> },
         { path: "token/login", element: <TokenLogin /> },
         { path: "nao-localizado", element: <NaoLocalizado404 /> },
         { path: "nao-autorizado", element: <NaoAutorizado401 /> },
